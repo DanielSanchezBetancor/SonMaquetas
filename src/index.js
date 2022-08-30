76ec77c37380716387c67fa984ecdfb7ab3cb109
DSanchez <danielsanchezpro@gmail.com>
2022-08-30 20:53:46 +0200

    @Routing @New folders and path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './templates/App';
+import App from './templates/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
---
+++ src/templates/App/App.js
@@ -0,0 +1,16 @@
+import Header from '../../components/Header.js';
+import Content from '../../components/Content/Content.js';
+import Footer from '../../components/Footer.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
