2661e67501ba8285014cf94570da3d28d610437e
DSanchez <danielsanchezpro@gmail.com>
2022-10-07 19:09:33 +0200

    @Components @Adding new Carousel

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,15 @@
-import FeaturedProductCarrousel from '../FeaturedProductCarrousel/FeaturedProductCarrousel';
 import CategoriesCardList from '../CategoriesCardList/CategoriesCardList';
 import AccessoriesGrid from '../AccessoriesGrid/AccessoriesGrid';
 import AddedValueCard from '../AddedValueCard/AddedValueCard.js';
+import Carousel from '../Carousel/Carousel.js';
+import PlanesImg from "../../assets/categories-plane.png";
+import ShipImg from "../../assets/categories-ship.png";
+import TrainImg from "../../assets/categories-train.png";
 
 function Content() {
     return (
         <>
-            <FeaturedProductCarrousel />
+            <Carousel images={[PlanesImg, ShipImg, TrainImg]}/>
             <CategoriesCardList />
             <AccessoriesGrid />
             <AddedValueCard />
