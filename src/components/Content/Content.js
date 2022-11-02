be56a84ff3c8b209d3ac1803d1a8647788be37d6
DSanchez <danielsanchezpro@gmail.com>
2022-11-02 13:14:44 +0100

    @Content @Erasing carousel as fade out untill figure out better solution

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,23 +3,13 @@ import FeaturedProducts from '../FeaturedProducts/FeaturedProducts.js';
 import HeroPage from '../HeroPage/HeroPage';
 /* Carousel images */
 import Carousel from '../Carousel/Carousel.js';
-import PlanesImg from "../../assets/categories-plane.png";
-import ShipImg from "../../assets/categories-ship.png";
-import TrainImg from "../../assets/categories-train.png";
-
 import './Content.css';
-import { useEffect, useState } from 'react';
 function Content() {
-    const [slide, setSlider] = useState(<HeroPage />);
-    useEffect(() => {
-        setTimeout(() => {
-            setSlider(<Carousel  images={[PlanesImg, ShipImg, TrainImg]}/>);
-        }, 5000)
-    })
     return (
         <section className='content'>
-            {slide}
+            <HeroPage />
             <CategoryCardList />
+            <Carousel />
             <FeaturedProducts />
         </section>
     )
