9b95c2d86ffbef31b6f8e7319e1478fba4b0d2ac
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-11-02 13:54:55 +0100

    Resolving conflict

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,18 +1,19 @@
-import CategoryCardList from '../CategoryCardList/CategoryCardList';
-import FeaturedProducts from '../FeaturedProducts/FeaturedProducts.js';
-import HeroPage from '../HeroPage/HeroPage';
+import CategoryCardList from "../CategoryCardList/CategoryCardList";
+import FeaturedProducts from "../FeaturedProducts/FeaturedProducts.js";
+import HeroPage from "../HeroPage/HeroPage";
 /* Carousel images */
-import Carousel from '../Carousel/Carousel.js';
-import './Content.css';
+import Carousel from "../Carousel/Carousel.js";
+import "./Content.css";
+
 function Content() {
-    return (
-        <section className='content'>
-            <HeroPage />
-            <CategoryCardList />
-            <Carousel />
-            <FeaturedProducts />
-        </section>
-    )
+  return (
+    <section className="content">
+      <HeroPage />
+      <CategoryCardList />
+      <Carousel />
+      <FeaturedProducts />
+    </section>
+  );
 }
 
 export default Content;
