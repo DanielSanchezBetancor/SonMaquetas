bd01b61a4a7e83146fb292282a517ff002ba38ef
DSanchez <danielsanchezpro@gmail.com>
2022-11-02 13:15:24 +0100

    @Content @New components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <ul className="footer__list">
+                <li className="footer__list__item">Quiénes somos</li>
+                <li className="footer__list__item">Blog</li>
+                <li className="footer__list__item">Información Copyright</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
