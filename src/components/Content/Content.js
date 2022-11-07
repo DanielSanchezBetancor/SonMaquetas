f2af57076befe109f174e32d86b357cd87c93d58
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-11-07 14:55:03 +0100

    @FeaturedProductsFixes @Fixes: Added Link to Featured Products | Added eu symbol | Added see less button

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,6 +2,7 @@ import CategoryCardList from "../CategoryCardList/CategoryCardList";
 import FeaturedProducts from "../FeaturedProducts/FeaturedProducts.js";
 import HeroPage from "../HeroPage/HeroPage";
 /* Carousel images */
+<<<<<<< Updated upstream
 import Carousel from "../Carousel/Carousel.js";
 import UsuallyTogether from "../UsuallyTogether/UsuallyTogether";
 import "./Content.css";
@@ -12,6 +13,27 @@ function Content() {
       <HeroPage />
       <CategoryCardList />
       <Carousel />
+=======
+import PlanesImg from "../../assets/categories-plane.png";
+import ShipImg from "../../assets/categories-ship.png";
+import TrainImg from "../../assets/categories-train.png";
+import Carousel from "../Carousel/Carousel.js";
+
+import { useEffect, useState } from "react";
+import UsuallyTogether from "../UsuallyTogether/UsuallyTogether";
+import "./Content.css";
+function Content() {
+  const [slide, setSlider] = useState(<HeroPage />);
+  useEffect(() => {
+    setTimeout(() => {
+      setSlider(<Carousel images={[PlanesImg, ShipImg, TrainImg]} />);
+    }, 5000);
+  });
+  return (
+    <section className="content">
+      {slide}
+      <CategoryCardList />
+>>>>>>> Stashed changes
       <FeaturedProducts />
       <UsuallyTogether />
     </section>
--- src/components/FeaturedProducts/FeaturedProductsItem/FeaturedProductsItem.js
+++ src/components/FeaturedProducts/FeaturedProductsItem/FeaturedProductsItem.js
@@ -1,21 +1,33 @@
 import PlanesImg from "../../../assets/categories-plane.png";
-import './FeaturedProductsItem.css';
+import "./FeaturedProductsItem.css";
 
-function FeaturedProductsItem({isEven, imgSrc, imgAlt, name, description, price}) {
-    return (
-        <div className={"featured-products-item " + ((isEven) ? ' featured-even' : '')}>
-            <div className="featured-products-item__img">
-                <img src={imgSrc} alt={imgAlt} />
-            </div>
-            <div className="featured-products-item__information">
-                <label className="featured-products-item__information__title">
-                    {name}
-                </label>
-                <label>{description}</label>
-                <label>{price}</label>
-            </div>
+function FeaturedProductsItem({
+  isEven,
+  imgSrc,
+  imgAlt,
+  name,
+  description,
+  price,
+  url,
+}) {
+  return (
+    <a href={url}>
+      <div
+        className={"featured-products-item " + (isEven ? " featured-even" : "")}
+      >
+        <div className="featured-products-item__img">
+          <img src={imgSrc} alt={imgAlt} />
         </div>
-    );
+        <div className="featured-products-item__information">
+          <label className="featured-products-item__information__title">
+            {name}
+          </label>
+          <label>{description}</label>
+          <label>{price}€</label>
+        </div>
+      </div>
+    </a>
+  );
 }
 
 export default FeaturedProductsItem;
