9a2f3fef98718bcc2245ecb742c51a6222d34a23
DSanchez <danielsanchezpro@gmail.com>
2022-11-08 12:16:39 +0100

    @Content @Cleaning and applying final components

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,30 +1,21 @@
+// Components
 import CategoryCardList from "../CategoryCardList/CategoryCardList";
-import FeaturedProducts from "../FeaturedProducts/FeaturedProducts.js";
+import FeaturedProducts from "../FeaturedProducts/FeaturedProducts";
 import HeroPage from "../HeroPage/HeroPage";
-/* Carousel images */
-import PlanesImg from "../../assets/categories-plane.png";
-import ShipImg from "../../assets/categories-ship.png";
-import TrainImg from "../../assets/categories-train.png";
-import Carousel from "../Carousel/Carousel.js";
-
-import { useEffect, useState } from "react";
+import Carousel from "../Carousel/Carousel";
 import UsuallyTogether from "../UsuallyTogether/UsuallyTogether";
+// Styles
 import "./Content.css";
 function Content() {
-  const [slide, setSlider] = useState(<HeroPage />);
-  useEffect(() => {
-    setTimeout(() => {
-      setSlider(<Carousel images={[PlanesImg, ShipImg, TrainImg]} />);
-    }, 5000);
-  });
-  return (
-    <section className="content">
-      {slide}
-      <CategoryCardList />
-      <FeaturedProducts />
-      <UsuallyTogether />
-    </section>
-  );
+    return (
+        <section className="content">
+            <HeroPage />
+            <CategoryCardList />
+            <Carousel />
+            <FeaturedProducts />
+            <UsuallyTogether />
+        </section>
+    );
 }
 
 export default Content;
