b07e14fb1cd1e0798314c5ac2d1fa214d383fc8a
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-11-08 12:05:10 +0100

    @General @Resolving conflict

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,18 +2,6 @@ import CategoryCardList from "../CategoryCardList/CategoryCardList";
 import FeaturedProducts from "../FeaturedProducts/FeaturedProducts.js";
 import HeroPage from "../HeroPage/HeroPage";
 /* Carousel images */
-<<<<<<< Updated upstream
-import Carousel from "../Carousel/Carousel.js";
-import UsuallyTogether from "../UsuallyTogether/UsuallyTogether";
-import "./Content.css";
-
-function Content() {
-  return (
-    <section className="content">
-      <HeroPage />
-      <CategoryCardList />
-      <Carousel />
-=======
 import PlanesImg from "../../assets/categories-plane.png";
 import ShipImg from "../../assets/categories-ship.png";
 import TrainImg from "../../assets/categories-train.png";
@@ -33,7 +21,6 @@ function Content() {
     <section className="content">
       {slide}
       <CategoryCardList />
->>>>>>> Stashed changes
       <FeaturedProducts />
       <UsuallyTogether />
     </section>
