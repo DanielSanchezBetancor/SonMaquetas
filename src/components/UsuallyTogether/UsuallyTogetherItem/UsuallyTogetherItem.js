1095c0396ebef0e404c056af135ead5d22f7883e
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-11-10 10:44:42 +0100

    @UsuallyTogetherStyle @Fix items centered in usually together category

--- src/components/UsuallyTogether/UsuallyTogetherItem/UsuallyTogetherItem.js
+++ src/components/UsuallyTogether/UsuallyTogetherItem/UsuallyTogetherItem.js
@@ -1,34 +1,38 @@
 import "./UsuallyTogetherItem.css";
 
 function UsuallyTogetherItem({
-    imgSrcFirstProduct,
-    imgSrcSecondProduct,
-    nameFirstProduct,
-    nameSecondProduct,
-    groupDescription,
-    groupPrice,
+  imgSrcFirstProduct,
+  imgSrcSecondProduct,
+  nameFirstProduct,
+  nameSecondProduct,
+  groupDescription,
+  groupPrice,
 }) {
-    return (
-        <div className="usually-products-item usually-products-item__container">
-            <div className="usually-products-item__img usually-products-item__img1">
-                <img src={imgSrcFirstProduct} alt="itemPic" />
-            </div>
-            <div className="usually-products-item__img usually-products-item__img2">
-                <img src={imgSrcSecondProduct} alt="itemPic" />
-            </div>
-            <div className="usually-products-item__information">
-                <p className="usually-products-item__information__title">
-                    {nameFirstProduct}
-                </p>
-                <p className="usually-products-item__information__price">+</p>
-                <p className="usually-products-item__information__title">
-                    {nameSecondProduct}
-                </p>
-                <p>{groupDescription}</p>
-                <p className="usually-products-item__information__price">{groupPrice}€</p>
-            </div>
+  return (
+    <div className="usually-products-item usually-products-item__container">
+      <div className="usually-products-item__img--container">
+        <div className="usually-products-item__img usually-products-item__img1">
+          <img src={imgSrcFirstProduct} alt="itemPic" />
         </div>
-    );
+        <div className="usually-products-item__img usually-products-item__img2">
+          <img src={imgSrcSecondProduct} alt="itemPic" />
+        </div>
+      </div>
+      <div className="usually-products-item__information">
+        <p className="usually-products-item__information__title">
+          {nameFirstProduct}
+        </p>
+        <p className="usually-products-item__information__price">+</p>
+        <p className="usually-products-item__information__title">
+          {nameSecondProduct}
+        </p>
+        <p>{groupDescription}</p>
+        <p className="usually-products-item__information__price">
+          {groupPrice}€
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default UsuallyTogetherItem;
