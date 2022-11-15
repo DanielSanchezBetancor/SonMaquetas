247722c3f82fdd1d36dad1f259002b02b0a4b778
Adrian Mayor <adrianmayorsantana@gmail.com>
2022-11-15 14:34:18 +0100

    @GeneralFinalFixes @fixes: card usually together fixed | footer with sentence

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,25 @@
 import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <ul className="footer__list">
-                <li className="footer__list__item">Quiénes somos</li>
-                <li className="footer__list__item">Blog</li>
-                <li className="footer__list__item">Información Copyright</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <ul className="footer__list">
+        {/*         <li className="footer__list__item">Quiénes somos</li>
+        <li className="footer__list__item">Blog</li>
+        <li className="footer__list__item">Información Copyright</li> */}
+
+        <p>
+          Powered with 🍨 by{" "}
+          <a
+            href="mailto:codingflavour@gmail.com"
+            className="footer__list__item"
+          >
+            Coding Flavour
+          </a>
+        </p>
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
